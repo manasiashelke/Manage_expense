d41218aef252f1d972de126eecb797a7c1df2dc9
Manasi Shelke <manasis924@gmail.com>
2024-03-05 18:23:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Card from './Components/Card';;
+import Card from './Components/Card';
 import Expenses from "./Components/Expenses";
 import NewExpenses from "./Components/New Expense/NewExpenses";
 
